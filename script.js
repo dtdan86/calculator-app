b2b0075a9d47eb2b0eff718ebeaa6b5ef56d3427
[Thanh Do] <[dtdan090286@gmail.com]>
2024-09-07 18:49:11 -0500

    Init repo

--- script.js
+++ script.js
@@ -2,21 +2,29 @@
 // Named function for addition
 function add(a, b) {
     // TODO: Implement addition
+    return a + b;
 }
 
 // Anonymous function for subtraction
 const subtract = function(a, b) {
     // TODO: Implement subtraction
+    return a - b;
 };
 
 // Arrow function for multiplication
 const multiply = (a, b) => {
     // TODO: Implement multiplication
+    return a * b;
 };
 
 // Named function for division
 function divide(a, b) {
     // TODO: Implement division with check for division by zero
+    if (b == 0) {
+        return 'Invalid Operation';
+    } else {
+        return a / b;
+    }
 }
 
 // Calculator object with methods
@@ -26,10 +34,12 @@ const calculator = {
 
     updateEquation: function(value) {
         // TODO: Implement update equation logic
+        this.equation += value;
         document.getElementById('equation').value = this.equation;
     },
     clearEquation: function() {
         // TODO: Implement clear equation logic
+        this.equation = '';
         document.getElementById('equation').value = this.equation;
     },
     calculate: function() {
@@ -41,16 +51,16 @@ const calculator = {
 
         switch (operation) {
             case '+':
-                result = // TODO: Implement call to add method using `this`
+                result = add(a, b);
                 break;
             case '-':
-                result = // TODO: Implement call to subtract method using `this`
+                result = subtract(a, b);
                 break;
             case '*':
-                result = // TODO: Implement call to multiply method using `this`
+                result = multiply(a, b);
                 break;
             case '/':
-                result = // TODO: Implement call to divide method using `this`
+                result = divide(a, b);
                 break;
             default:
                 result = 'Invalid Operation';
@@ -65,21 +75,25 @@ document.querySelectorAll('button[name="input"]').forEach(button => {
     button.addEventListener('click', (event) => {
         const value = event.target.value;
         // TODO: Add call to updateEquation and pass value as an argument
+        calculator.updateEquation(value);
     });
 });
 
 document.querySelectorAll('button[name="operation"]').forEach(button => {
     button.addEventListener('click', (event) => {
-        const value = event.target.value;
+        const value = ' ' + event.target.value + ' ';
         // TODO: Add call to updateEquation and pass value as an argument
         // NOTE: This passes the selected operation as part of a text string
+        calculator.updateEquation(value);
     });
 });
 
 document.querySelector('button[name="calculate"]').addEventListener('click', () => {
     // TODO: Add call to calculate() result and update display
+    calculator.calculate();
 });
 
 document.querySelector('button[name="clear"]').addEventListener('click', () => {
     // TODO: Clear the display
+    calculator.clearEquation();
 });
